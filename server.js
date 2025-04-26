dc874dd72845ee4c287f49b815dfc84d600fe0b3
GouthamSomaraju <mr.goutham00@gmail.com>
2025-04-26 14:27:09 +0530

    Updated

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 // Create DB Connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}).promise();
+
 
 // Connect to MySQL
 db.connect(err => {
